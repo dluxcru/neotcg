4bd61a4f3ba8fbd64740ac66f5260703686fcd89
David Dale <daviddale.webmaster@gmail.com>
2023-06-24 17:19:56 -0500

    Let's try selenium instead

--- Python_learning/tcgparser.py
+++ Python_learning/tcgparser.py
@@ -1,14 +1,24 @@
 import ssl
-
-from bs4 import BeautifulSoup
 import requests
-##The orginal Neopets JS code for opening a card.
-#function showCard(EDID, CARDID) {
-				# window.open("displayCard.phtml?edid=" + EDID + "&id=" + CARDID, "CardDisplay", "width=300, height=420, location=no, status=no");
-			# };
+import html
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+
+
 from Python_learning.Card import Card
 from Python_learning.NetworkHelpers.NeoTLSConnector import NeoTLSConnector
 from Python_learning.TableCrawler import TableCrawler
+
+"""The orginal Neopets JS code for opening a card.
+function showCard(EDID, CARDID) {
+				 window.open("displayCard.phtml?edid=" + EDID + "&id=" + CARDID, "CardDisplay", "width=300, height=420, location=no, status=no");
+			 };
+"""
 
 with open("NeopetsTCGChecklist.html", "r", encoding="utf-8") as neotcg:
     doc = BeautifulSoup(neotcg, "html.parser")
@@ -17,20 +27,17 @@ with open("NeopetsTCGChecklist.html", "r", encoding="utf-8") as neotcg:
 # tables.crawl()
 
 wandConfusion = Card(232, 1, "Wand of Confusion", "R")
-print(ssl.OPENSSL_VERSION)
 
-# session = requests.Session()
-# session.verify = False
-# session.mount("https://", NeoTLSConnector())
-#
-# response = session.get(wandConfusion.showCard())
-# session = requests.Session()
-# session.verify = False
-# session.mount("https://", requests.adapters.HTTPAdapter(pool_connections=1, pool_maxsize=1, max_retries=3, pool_block=False, ssl_version=ssl.PROTOCOL_TLSv1_2))
-#
-# response = session.get(wandConfusion.showCard())
+# r = requests.get(wandConfusion.showCard(), verify=False)
+# # print(r.url)
+# # print(r.text)
+# cardHTML = r.text
+# formatted = html.escape(cardHTML)
+# # print(cardHTML)
+## Code Base: https://www.youtube.com/watch?v=SPM1tm2ZdK4
+options = Options()
+options.add_experimental_option("detatch", True)
 
-r = requests.get(wandConfusion.showCard(),verify=False)
-print(r.url)
-
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(wandConfusion.showCard())
 
